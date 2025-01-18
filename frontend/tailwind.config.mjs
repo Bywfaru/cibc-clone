dcd4a2eaec2c10a52a5fa77eb52cf9d2c97fa632
William He <contact@williamhe.dev>
2025-01-18 00:12:23 -0800

    chore: init commit

---
+++ frontend/tailwind.config.mjs
@@ -0,0 +1,14 @@
+// noinspection JSUnusedGlobalSymbols
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    colors: {
+      primary: '#C41F3E',
+      secondary: '#8B1D41',
+      'background-primary': '#F4F4F4',
+      'background-secondary': '#383b3e',
+    },
+  },
+  plugins: [],
+}
